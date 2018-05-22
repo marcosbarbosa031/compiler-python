38ad75011eff2124b2b4fb09c96ce56a7ecac024
Marcos <ausdhahd@gmail.com>
2018-05-22 15:43:46 -0300

    implemented functions in stack

--- test.py
+++ test.py
@@ -4,18 +4,26 @@ from utils import SymbolTable
 s = Stack()
 t = SymbolTable("8.25", "float", 0)
 
+if(s.isEmpty()):
+  print('vazio')
+
+
 s.push(t)
 
 t = SymbolTable("5", "int", 0)
 
 s.push(t)
 
+t = SymbolTable("a", "char", 1)
+
+s.push(t)
+
+print('antes')
 s.printTable()
-x = s.pop()
+
+s.removeScope(0)
+# s.clearTable()
+
+print('depois')
 
 s.printTable()
-
-
-s.top().getTable()
-
-x.getTable()
--- utils/stack/stack.py
+++ utils/stack/stack.py
@@ -17,5 +17,29 @@ class Stack(object):
     
     def top (self):
         return self.table[-1]
+
+    def searchScope(self, symbolT):
+        for t in self.table:
+            if (t.scope == symbolT.scope and t.lex == symbolT.lex):
+                return True
+        return False
     
+    def serchAll (self, symbolT):
+        for t in self.table:
+            if (t.lex == symbolT.lex):
+                return True
+        return False
+    
+    def isEmpty (self):
+        if not self.table:
+            return True
+        return False
+    
+    def removeScope (self, scope):
+        self.table = [t for t in self.table if t.scope != scope]
+        # return self.table
+
+    def clearTable (self):
+        del self.table[:]
+
     pass
