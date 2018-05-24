c0247e382796560cbd9b4cc7ee4de3e5a220353e
Marcos Barbosa <enos_mm@hotmail.com>
2018-05-23 21:59:38 -0300

    removing pyc files

--- print_err.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-class PrintErr(object):
-
-    @staticmethod
-    def print_error(token, msg):
-        print("ERRO na linha {0}, coluna {1}, ultimo token lido '{2}': {3}".format(token['ln'], token['cl'], token['lex'], msg))
-        sys.exit(1)
